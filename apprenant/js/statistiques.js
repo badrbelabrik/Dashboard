56ad37e1f76cbeac6d3fe79853f0d6158bcdea10
badrbelabrik <badr.belabrik@gmail.com>
2025-12-31 12:40:44 +0100

    create statistiques page

---
+++ apprenant/js/statistiques.js
@@ -0,0 +1,24 @@
+const menuToggle = document.getElementById('sidebarToggle');
+const sidebar = document.getElementById('sidebar');
+const sidebarOverlay = document.getElementById('sidebarOverlay');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('show');
+  sidebarOverlay.classList.toggle('show');
+});
+
+sidebarOverlay.addEventListener('click', () => {
+  sidebar.classList.remove('show');
+  sidebarOverlay.classList.remove('show');
+});
+
+ 
+if (window.innerWidth <= 768) {
+  const navLinks = sidebar.querySelectorAll('.nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      sidebar.classList.remove('show');
+      sidebarOverlay.classList.remove('show');
+    });
+  });
+}
